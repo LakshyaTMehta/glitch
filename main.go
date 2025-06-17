390dff97c9a27b117cd5b8ef9bd9a10dc7c47804
Lakshya T Mehta <131202380+LakshyaTMehta@users.noreply.github.com>
2025-06-17 21:44:03 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,30 +3,27 @@ package main
 import(
 	"fmt"
 	"os"
-	"time"
 	"math"
-	"glitch/pkgs/animation"
+  "time"
+	"glitch/pkgs/render"
 	"github.com/gdamore/tcell/v2"
 )
 
-var startTime time.Time
-
-type CircleTracer struct {
-	centreX, centreY, radius, speed, initialAngle float64
+type Circle struct {
+  radius, center_x, center_y float64
+  resolution int
 }
 
-func (c CircleTracer) PosWhen( t time.Duration ) (x, y float64) {
-	x = c.radius * math.Cos( t.Seconds() * 2 * math.Pi * c.speed + c.initialAngle) + c.centreX
-	y = c.radius * math.Sin( t.Seconds() * 2 * math.Pi * c.speed + c.initialAngle) + c.centreY
-	return 
+func (c Circle) vertices() (out []render.Point) {
+  step := math.Pi * 2 / float64(c.resolution)
+  out = make([]render.Point, c.resolution)
+  for i := 0 ; i < c.resolution; i++ {
+    x := c.radius * math.Cos(step * float64(i)) + c.center_x
+    y := c.radius * math.Sin(step * float64(i)) + c.center_y
+    out[i] = render.Point{X: int(x), Y: int(y)}
+  }
+  return out
 }
-
-func draw( s tcell.Screen, e animation.Element ) {
-	_, h := s.Size()
-	x, y := e.PosWhen(time.Since(startTime))
-	s.SetContent( int(x), h - int(y), tcell.RuneBlock, nil, tcell.StyleDefault)
-}
-
 
 func main() {
 	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
@@ -40,16 +37,14 @@ func main() {
 			fmt.Println("Screen not initialised")
 	}
 	defer s.Fini()
-
+  
+  w, h := s.Size()
 	s.SetStyle(defStyle)
-
-	//point := NewParticle(0, 0, 0, 0, 1, 0.8)
-	//point.Push(Vector2D{10, 10})
-
-	c := CircleTracer{ 80, 20, 18, 1, 0 }
-	c1 := CircleTracer{ 80, 20, 18, 1, 0.2 }
-	c2 := CircleTracer{ 80, 20, 18, 1, 0.4 }
-	c3 := CircleTracer{ 80, 20, 18, 1, 0.6 }
+  c := Circle{  radius: 5,
+                center_x: float64(w) / 2,
+                center_y: float64(h) / 2,
+                resolution: 6,
+              }
 
 	go func () {
 		for ;; {
@@ -59,22 +54,29 @@ func main() {
 					switch ev.Key() {
 						case tcell.KeyCtrlC:
 							os.Exit(0)
-						case tcell.KeyUp:
-							c.speed += .1
-						case tcell.KeyDown:
-							c.speed -= .1
+            case tcell.KeyUp:
+              c.resolution += 1
+            case tcell.KeyDown:
+              c.resolution -= 1
 					}
 			}
 		}
 	}()
 	
-	startTime = time.Now()
+  go func () {
+    scaling_factor := 10.0
+    for i := 0.0;; i += 0.05 {
+      c.radius = scaling_factor * math.Cos(i)
+      if i >= math.Pi * 2 {
+        i = 0
+      }
+      time.Sleep(time.Millisecond * 10)
+    }
+  }()
+
 	for ;; {
 		s.Clear()
-		draw(s, c)
-		draw(s, c1)
-		draw(s, c2)
-		draw(s, c3)
+    render.Render(c.vertices(), true, s, defStyle)
 		s.Show()
 	}
 }
